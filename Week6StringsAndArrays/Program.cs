f808008ed9f8d94cb2a4574e62024a0e6ea048f8
Elisabeth Teearu <elisabeth282000@gmail.com>
2024-10-11 14:38:12 +0300

    week 6

---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,21 @@
+﻿
+string userName = "Elisabeth";
+
+Console.WriteLine(userName.Length);
+Console.WriteLine($"The last letter in your name is {userName[userName.Length - 1]}.");
+
+string updatedUsername = userName.Replace('a', '4').Replace('e', '3');
+
+Console.WriteLine(updatedUsername);
+
+//int counter = 0;
+
+//for (int i = 0; i < userName.Length; i++)
+//{
+//    if (userName[i] == 'a')
+//    {
+//        counter++;
+//    }
+//}
+
+//Console.WriteLine($"Your first name has {counter} a-letters.");
